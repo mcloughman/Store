3ef9a89531d55e47eebfa12e3a55d23e47849d1b
Michael Loughman <mcloughman@gmail.com>
2021-11-06 14:44:50 -0700

    Add multer middleware to accommodate file upload and incorporate it into products new post route

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const { validationResult } = require("express-validator");
+const multer = require("multer");
+
 const productsRepo = require("../../repositories/products");
 const productsNewTemplate = require("../../views/admin/products/new");
 const { requireTitle, requirePrice } = require("./validators");
 
 const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.get("/admin/products", async (req, res) => {
   res.send("Products");
@@ -17,10 +20,13 @@ router.get("/admin/products/new", (req, res) => {
 router.post(
   "/admin/products/new",
   [requireTitle, requirePrice],
+  upload.single("image"),
   async (req, res) => {
     const errors = validationResult(req);
-    console.log(errors);
+    const image = req.file.buffer.toString("base64");
+    const { title, price } = req.body;
     res.send("Submitted");
+    await productsRepo.create({ title, price, image });
   }
 );
 
--- views/admin/products/new.js
+++ views/admin/products/new.js
@@ -4,7 +4,7 @@ const { getError } = require("../../helpers");
 module.exports = ({ errors }) => {
   return layout({
     content: `
-            <form method="POST">
+            <form method="POST" enctype="multipart/form-data">
             <input type="text" name="title" placeholder="Title" />
             <input type="text" name="price" placeholder="Price" />
             <input type="file" name="image" />
