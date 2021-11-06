4ddec25b11737732fb08ca07ca678013b4d50e0e
Michael Loughman <mcloughman@gmail.com>
2021-11-05 20:42:52 -0700

    Add public directory with css also link to bootstrap and css in the layout file. Since it's just admin signup, not worried about styling much. Better styling ahead for non admin templates

--- views/admin/auth/signin.js
+++ views/admin/auth/signin.js
@@ -4,13 +4,29 @@ const { getError } = require("../../helpers.js");
 module.exports = ({ errors }) => {
   return layout({
     content: `
-    <form method="POST">
-      <input type="email" name="email" placeholder="Email" />
-      ${getError(errors, "email")}
-      <input type="password" name="password" placeholder="Password" />
-      ${getError(errors, "password")}
-      <button>Sign In</button>
-    </form>
-    `,
+    <div class="container">
+      <div class="row">
+        <div class="col">
+        </div>
+        <div class="col-lg-8">
+          <form method="POST">
+            <div class="mb-3">
+            <h1 class="text-center h1">Admin Login</h1>
+            
+             <input class="form-control mt-5" type="email" name="email" placeholder="Email" />
+             ${getError(errors, "email")}
+             
+           </div>
+           <div class="mb-3">
+            <input class="form-control" type="password" name="password" placeholder="Password" />
+             ${getError(errors, "password")}
+            <button class="mt-3 text-center">Sign In</button>
+           </div>
+         </form>
+        </div>
+        <div class="col">
+        </div>
+      </div>
+    </div>`,
   });
 };
--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -6,17 +6,34 @@ module.exports = ({ req, errors }) => {
   // destructure out the req object
   return layout({
     content: `
-    <div>
+    
     Your id is: ${req.session.userId}
-    <form method="POST">
-      <input type="email" placeholder="email" name="email"/>
-      ${getError(errors, "email")}
-      <input type="password" placeholder="password" name="password"/>
-      ${getError(errors, "password")}
-      <input type="password" placeholder="password confirmation" name="passwordConfirmation"/>
-      ${getError(errors, "passwordConfirmation")}
-      <button>Submit</button>
-    </form>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+        </div>
+        <div class="col-lg-8">
+
+          <form method="POST">
+            <div class="mb-3">
+            <h1 class="text-center h1">Admin Sign Up</h1>
+              <input type="email" class="form-control" placeholder="email" name="email"/>
+              ${getError(errors, "email")}
+            </div>
+            <div class="mb-3">
+              <input type="password" class="form-control" placeholder="password" name="password"/>
+              ${getError(errors, "password")}
+            </div>
+            <div class="mb-3">
+              <input type="password" class="form-control" placeholder="password confirmation" name="passwordConfirmation"/>
+              ${getError(errors, "passwordConfirmation")}
+            </div>
+            <button>Submit</button>
+          </form>
+        </div>
+        <div class="col">
+        </div>
+      </div>
     </div>
     `,
   });
