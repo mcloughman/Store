2d18169c5921154d3a253861094e217a67b6cf15
Michael Loughman <mcloughman@gmail.com>
2021-11-08 11:02:01 -0800

    Add code to carts/products post route to create a cart if it doen't already exist. And also to add items to the cart either increasing the quantity if the f the item is already present in the cart or initial add of that product to the cart.

--- routes/carts.js
+++ routes/carts.js
@@ -1,10 +1,35 @@
 const express = require("express");
+const cartsRepo = require("../repositories/carts");
 
 const router = express.Router();
 
 // receive post request to add an item to a cart
-router.post("/cart/products", (req, res) => {
+router.post("/cart/products", async (req, res) => {
   console.log(req.body.productId);
+  // Either increment quantity or add new product to items array
+  let cart;
+  if (!req.session.cartId) {
+    // We don't have a cart and need to create one
+    // and store the id on the req.session.cartId
+    // property
+    cart = await cartsRepo.create({ items: [] });
+    req.session.cartId = cart.id;
+  } else {
+    // we have a cart. Need to get it from the repository
+    cart = await cartsRepo.getOne(req.session.cartId);
+  }
+  const existingItem = cart.items.find(
+    (item) => item.id === req.body.productId
+  );
+  if (existingItem) {
+    // increment quantity
+    existingItem.quantity++;
+  } else {
+    cart.items.push({ id: req.body.productId, quantity: 1 });
+  }
+  await cartsRepo.update(cart.id, {
+    items: cart.items,
+  });
   res.send("Product Added to Cart");
 });
 
