d5cc87643f2b1c60fb0158f0825cbb77e3504f36
Michael Loughman <mcloughman@gmail.com>
2021-11-08 11:19:13 -0800

    Create the get route handler in cart routes. Create the show page to show the items in the cart

--- routes/carts.js
+++ routes/carts.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cartsRepo = require("../repositories/carts");
+const productsRepo = require("../repositories/products");
+const cartShowTemplate = require("../views/carts/show");
 
 const router = express.Router();
 
@@ -34,6 +36,18 @@ router.post("/cart/products", async (req, res) => {
 });
 
 // receive get request to show the items in the cart
+
+router.get("/cart", async (req, res) => {
+  if (!req.session.cartId) {
+    return res.redirect("/");
+  }
+  const cart = await cartsRepo.getOne(req.session.cartId);
+  for (let item of cart.items) {
+    const product = await productsRepo.getOne(item.id);
+    item.product = product;
+  }
+  res.send(cartShowTemplate({ items: cart.items }));
+});
 
 // receive post request to delete an item
 
